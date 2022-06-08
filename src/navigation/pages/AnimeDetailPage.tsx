03e26c4ae8ffbbfd24c32064b46da3df65778f28
Omar Jbara <omar.jbara2@gmail.com>
2022-06-08 14:00:22 +0200

    feat: scale on press image

--- src/navigation/pages/AnimeDetailPage.tsx
+++ src/navigation/pages/AnimeDetailPage.tsx
@@ -1,7 +1,13 @@
 import type { ReactElement } from 'react'
-import { ActivityIndicator } from 'react-native'
+import { ActivityIndicator, Dimensions, TouchableOpacity } from 'react-native'
 import { Heading, Image, Text, VStack } from 'native-base'
 import { useQuery } from 'react-query'
+import Animated, {
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated'
+import { useState } from 'react'
 import { JikanClient } from '../../api/jikan/client'
 import { Page } from '../../shared/layout/Page'
 import { ShowMore } from '../../shared/components/ShowMore'
@@ -15,6 +21,13 @@ export function AnimeDetailPage({
   const { status, data: anime } = useQuery(['anime', id], () =>
     JikanClient.getAnime(id),
   )
+  const [imageHovered, setImageHovered] = useState(false)
+  const animatedImageStyle = useAnimatedStyle(() => ({
+    height: withSpring(imageHovered ? 600 : 180, {
+      damping: 67,
+    }),
+    width: '100%',
+  }))
 
   return (
     <Page px={0} isScrollView>
@@ -22,12 +35,12 @@ export function AnimeDetailPage({
       {status === 'error' ? <Text color="red.300">Error</Text> : null}
       {status === 'success' && anime ? (
         <VStack>
-          <Image
-            width="100%"
-            height={180}
-            alt={anime.title}
-            source={{ uri: anime.images.jpg.large_image_url }}
-          />
+          <TouchableOpacity onPress={() => setImageHovered(v => !v)}>
+            <Animated.Image
+              style={animatedImageStyle}
+              source={{ uri: anime.images.jpg.large_image_url }}
+            />
+          </TouchableOpacity>
           <VStack space={4} p={4}>
             <AnimeMetrics anime={anime} />
             <VStack space={2}>
