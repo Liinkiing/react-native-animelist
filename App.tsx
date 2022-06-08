7d90fb721188a861d0cdd0aed497440e5c81cc41
Omar Jbara <omar.jbara2@gmail.com>
2022-06-08 15:06:29 +0200

    feat: add deep link

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { NativeBaseProvider } from 'native-base'
+import * as Linking from 'expo-linking'
+import { ActivityIndicator } from 'react-native'
 import { AppNavigator } from './src/navigation/AppNavigator'
 import { AuthProvider } from './src/shared/providers/AuthProvider'
 
+const prefix = Linking.createURL('/')
 const queryClient = new QueryClient()
 
 export default function App() {
@@ -11,7 +14,18 @@ export default function App() {
     <NativeBaseProvider>
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
-          <NavigationContainer>
+          <NavigationContainer
+            linking={{
+              prefixes: [prefix],
+              config: {
+                screens: {
+                  Welcome: 'welcome',
+                  Login: 'login',
+                },
+              },
+            }}
+            fallback={<ActivityIndicator />}
+          >
             <AppNavigator />
           </NavigationContainer>
         </QueryClientProvider>
